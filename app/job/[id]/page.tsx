ec650513a816ac92d2cca3c805500f6c02faf104
21120321 <gamefun206@gmail.com>
2024-07-14 00:18:47 +0700

    fix server url

--- app/job/[id]/page.tsx
+++ app/job/[id]/page.tsx
@@ -10,7 +10,7 @@ interface DynamicProps {
 
 const getData = async (id: string) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_API}/post/${id}`,
+    `/api/post/${id}`,
     {
       cache: "no-store",
     }
